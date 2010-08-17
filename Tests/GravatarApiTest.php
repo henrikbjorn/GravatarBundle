0d16bdcdef69ffdbf1ed80f916ac2ccbe684342a
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-08-17 05:46:20 -0700

    Fix typo. What does this test exactly? I think assertEquals takes 2 args

--- Tests/GravatarApiTest.php
+++ Tests/GravatarApiTest.php
@@ -14,7 +14,7 @@ class GravatarApiTest extends TestCase
 
     public function testGravatarUrlWithDefaultImage()
     {
-        $this->assertEequals('http://www.gravatar.com/avatar/0aa61df8e35327ac3b3bc666525e0bee?s=80&r=g&d=mm');
+        $this->assertEquals('http://www.gravatar.com/avatar/0aa61df8e35327ac3b3bc666525e0bee?s=80&r=g&d=mm');
     }
 
 }
