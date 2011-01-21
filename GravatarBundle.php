d5a08aec5be7c059efe8b19dcc9fd2b29ce9ae3e
Antoine Hérault <antoine.herault@gmail.com>
2011-01-22 00:22:39 +0100

    Implement two new bundle interface methods

--- GravatarBundle.php
+++ GravatarBundle.php
@@ -6,4 +6,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class GravatarBundle extends BaseBundle
 {
+    public function getNamespace()
+    {
+        return __NAMESPACE__;
+    }
+
+    public function getPath()
+    {
+        return __DIR__;
+    }
 }
