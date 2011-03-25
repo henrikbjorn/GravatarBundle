09ca1aac07cf69a7309e46b951499df2b41209f1
ornicar <thibault.duplessis@gmail.com>
2011-03-25 11:43:56 -0700

    Fix DIC extension

--- DependencyInjection/GravatarExtension.php
+++ DependencyInjection/GravatarExtension.php
@@ -15,32 +15,14 @@ class GravatarExtension extends Extension
         $loader->load('config.xml');
 
         $parameters = array();
-        foreach (array('size', 'rating', 'default') as $key) {
-            if (isset($config[$key])) {
-                $parameters[$key] = $config[$key];
+        foreach ($configs as $config) {
+            foreach (array('size', 'rating', 'default') as $key) {
+                if (isset($config[$key])) {
+                    $parameters[$key] = $config[$key];
+                }
             }
         }
 
         $container->getDefinition('gravatar.api')->addArgument($parameters);
-    }
-
-    /**
-     * Returns the base path for the XSD files.
-     *
-     * @return string The XSD base path
-     */
-    public function getXsdValidationBasePath()
-    {
-        return null;
-    }
-
-    public function getNamespace()
-    {
-        return 'http://www.symfony-project.org/schema/dic/symfony';
-    }
-
-    public function getAlias()
-    {
-        return 'gravatar';
     }
 }
